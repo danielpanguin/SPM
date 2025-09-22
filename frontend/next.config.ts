b8efd72065af5f178d95e17ae17121911b773ead
ulteare <jonte.lee.2022@scis.smu.edu.sg>
2025-09-22 19:48:41 +0800

    shifted env.local to root dir and adjusted other code to call this env file correctly

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
+
+// Load environment variables from parent directory
+require('dotenv').config({ path: path.resolve('..', '.env.local') });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(".."),
+  },
 };
 
 export default nextConfig;
